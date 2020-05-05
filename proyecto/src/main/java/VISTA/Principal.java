f005e9347af86ba0a3535e6ab27d294f798da6d2
Antonio Ferrer <buceocv@gmail.comr>
2020-05-05 17:50:07 +0200

    Fecha en vista principal creada

--- proyecto/src/main/java/VISTA/Principal.java
+++ proyecto/src/main/java/VISTA/Principal.java
@@ -1,13 +1,25 @@
 package VISTA;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class Principal {
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+
+public class Principal implements Initializable{
 
     public Label lbltoday;
+
+    public void initialize(URL location, ResourceBundle resources) {
+        lbltoday.setText("Valencia a " + LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+    }
+
 
     public void openViewInsertPenalty(ActionEvent actionEvent) {
     }
