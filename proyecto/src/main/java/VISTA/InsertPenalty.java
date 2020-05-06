b069c67fb509d219074245e70bf1f84e0c2957a4
Antonio Ferrer <buceocv@gmail.comr>
2020-05-06 16:15:47 +0200

    Cambiado TestFx por FontAwesomeIcons Close #12

--- proyecto/src/main/java/VISTA/InsertPenalty.java
+++ proyecto/src/main/java/VISTA/InsertPenalty.java
@@ -5,7 +5,8 @@ import javafx.scene.Node;
 import javafx.stage.Stage;
 
 public class InsertPenalty {
-    public void closeView(ActionEvent actionEvent) {
+
+    public void btnClose(ActionEvent actionEvent) {
         Node source = (Node) actionEvent.getSource();
         Stage stage = (Stage) source.getScene().getWindow();
         stage.close();
