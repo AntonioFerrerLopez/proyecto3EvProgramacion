1683c58d0d4b76cf759a431e8ff45eef6f65d626
Antonio Ferrer <buceocv@gmail.comr>
2020-05-08 14:17:27 +0200

    Cargada la lista de policias en el tableview

--- proyecto/src/VIEW/INSERTPENALTIES/InsertPenalty.java
+++ proyecto/src/VIEW/INSERTPENALTIES/InsertPenalty.java
@@ -1,15 +1,23 @@
 package VIEW.INSERTPENALTIES;
 
+import DATA.DAO.PoliceDAO;
+import MODEL.Police;
+import VIEW.TOOLS.Alerts;
 import com.jfoenix.controls.JFXButton;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class InsertPenalty implements Initializable {
@@ -24,10 +32,37 @@ public class InsertPenalty implements Initializable {
     public JFXButton btnAccept;
     public JFXButton btnClean;
 
+    private List<Police> policesList ;
+    private ObservableList<Police> observablePolicesList ;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        if(obtainListOfPolicesOnDb()){
+            setupTableList();
+        }else{
+            Alerts.instanceOf().generateError("La lista de policias no esta disponible");
+        }
+    }
 
+    private void setupTableList() {
+        observablePolicesList = FXCollections.observableArrayList();
+        numPlateCol.setCellValueFactory(new PropertyValueFactory("policePlateNumber"));
+        nameCol.setCellValueFactory(new PropertyValueFactory("name"));
+        for (Police police: policesList){
+            observablePolicesList.add(police);
+        }
+        tvPoliceSelector.setItems(observablePolicesList);
+    }
 
+    private boolean obtainListOfPolicesOnDb() {
+        boolean listObteined = false;
+        try {
+            policesList = PoliceDAO.instanceOf().obtainAll();
+            listObteined = true;
+        } catch (SQLException errorSql) {
+            Alerts.instanceOf().generateWarningWithErrorCode(errorSql.getErrorCode() ,errorSql.getMessage());
+        }
+        return listObteined;
     }
 
     public void registerPenaltyOnDb(ActionEvent actionEvent) {
