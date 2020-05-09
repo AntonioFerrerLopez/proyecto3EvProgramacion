c8c5b34442745e03637663a632d9e67881b6171c
Antonio Ferrer <buceocv@gmail.comr>
2020-05-09 18:56:14 +0200

    Creada la vista inicial, comenzando con la codificación

--- proyecto/src/VIEW/LISTTRAFICFINE/ListOfTraficFines.java
+++ proyecto/src/VIEW/LISTTRAFICFINE/ListOfTraficFines.java
@@ -1,13 +1,45 @@
 package VIEW.LISTTRAFICFINE;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXListView;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
-public class ListOfTraficFines {
-    public void closeView(ActionEvent actionEvent) {
-        Node source = (Node) actionEvent.getSource();
-        Stage stage = (Stage) source.getScene().getWindow();
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ListOfTraficFines implements Initializable {
+
+    public JFXComboBox filterSelector;
+    public JFXListView lvPolicesList;
+    public TableView tableOfTraficFines;
+    public TableColumn colDate;
+    public TableColumn colPolice;
+    public TableColumn colNifInfractor;
+    public TableColumn colAmmount;
+    public TableColumn colPointRetired;
+    public Label lblTotalTraficFineAmmount;
+    public JFXButton btnBack;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+    public void filterChanged(ActionEvent actionEvent) {
+    }
+
+
+    public void BackToMain(ActionEvent actionEvent) {
+        Stage stage = (Stage) btnBack.getScene().getWindow();
         stage.close();
+
     }
 }
